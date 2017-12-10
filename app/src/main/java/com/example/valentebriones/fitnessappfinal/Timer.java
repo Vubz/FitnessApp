ee3956d6e49b377d4ff2dcf55a2f437465b930e5
Valente Briones <valentebriones@Valentes-MacBook-Pro.local>
2017-12-10 13:44:46 -0800

    Timer

--- app/src/main/java/com/example/valentebriones/fitnessappfinal/Timer.java
+++ app/src/main/java/com/example/valentebriones/fitnessappfinal/Timer.java
@@ -3,15 +3,32 @@ package com.example.valentebriones.fitnessappfinal;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Timer extends Fragment {
-
+    TextView textView;
+    Button start, pause, reset, lap;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
+    Handler handler;
+    int Seconds, Minutes, MilliSeconds;
+    ListView listView;
+    String[] ListElements = new String[]{};
+    List<String> ListElementsArrayList;
+    ArrayAdapter<String> Adapter;
 
     public Timer() {
         // Required empty public constructor
@@ -27,8 +44,108 @@ public class Timer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+        View View = inflater.inflate(R.layout.fragment_timer, container, false);
+        textView = (TextView) View.findViewById(R.id.textTimer);
+        start = (Button)View.findViewById(R.id.buttonStart);
+        pause = (Button)View.findViewById(R.id.buttonPause);
+        reset = (Button)View.findViewById(R.id.buttonReset);
+        lap = (Button)View.findViewById(R.id.buttonSaveLap);
+        listView = (ListView) View.findViewById(R.id.listview1);
+
+        handler = new Handler();
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        Adapter = new ArrayAdapter<String>(View.getContext(),
+                android.R.layout.simple_list_item_1,
+                ListElementsArrayList
+        );
+
+        listView.setAdapter(Adapter);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+
+                textView.setText("00:00:00");
+
+                ListElementsArrayList.clear();
+
+                Adapter.notifyDataSetChanged();
+            }
+        });
+
+        lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ListElementsArrayList.add(textView.getText().toString());
+
+                Adapter.notifyDataSetChanged();
+
+            }
+        });
+
+        return View;
     }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            textView.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+
+    };
 
 
 }
