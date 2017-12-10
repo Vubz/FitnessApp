d7163db5ec556a7c21f0b1aeada3e0e6dd3a482a
Paulina <pdelagarza@cetys.edu.mx>
2017-12-10 14:14:19 -0800

    Distance

--- app/src/main/java/com/example/valentebriones/fitnessappfinal/DistanceTraveled.java
+++ app/src/main/java/com/example/valentebriones/fitnessappfinal/DistanceTraveled.java
@@ -7,10 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 
 public class DistanceTraveled extends Fragment {
+    TextView distance;
 
     public DistanceTraveled() {
         // Required empty public constructor
@@ -26,7 +27,9 @@ public class DistanceTraveled extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_distance_traveled, container, false);
+        View vista = inflater.inflate(R.layout.fragment_distance_traveled, container, false);
+        distance = (TextView) vista.findViewById(R.id.textDistance);
+        return vista;
     }
 
 }
