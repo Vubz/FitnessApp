80740fb8bb7f1d7d01b0755535b8ec633bd6a2b6
Paulina <pdelagarza@cetys.edu.mx>
2017-12-10 17:23:53 -0800

    Speed/Manifest Issues

--- app/src/main/java/com/example/valentebriones/fitnessappfinal/DistanceTraveled.java
+++ app/src/main/java/com/example/valentebriones/fitnessappfinal/DistanceTraveled.java
@@ -1,17 +1,30 @@
 package com.example.valentebriones.fitnessappfinal;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 
-public class DistanceTraveled extends Fragment {
+public class DistanceTraveled extends Fragment implements LocationListener {
     TextView distance;
+
 
     public DistanceTraveled() {
         // Required empty public constructor
@@ -23,13 +36,53 @@ public class DistanceTraveled extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_distance_traveled, container, false);
         distance = (TextView) vista.findViewById(R.id.textDistance);
+
+
+        LocationManager lm = (LocationManager) this.getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this.getActivity());
+
+            this.onLocationChanged(null);
+
+
         return vista;
     }
 
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+
+        if(location==null){
+            distance.setText("-.- m/s");
+        }
+        else
+        {
+            float nCurrentSpeed = location.getSpeed();
+            distance.setText(nCurrentSpeed+ " m/s");
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
